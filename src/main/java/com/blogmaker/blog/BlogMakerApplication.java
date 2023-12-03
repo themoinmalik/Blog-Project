7203eb6f2299f0ad22e54e282a0749c7cc0dadc5
Moin Malik <themoinmalik@gmail.com>
2023-12-03 11:30:32 +0530

    master: new Blog project setup

---
+++ src/main/java/com/blogmaker/blog/BlogMakerApplication.java
@@ -0,0 +1,13 @@
+package com.blogmaker.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogMakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogMakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blogmaker/blog/BlogMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogmaker.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogMakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
