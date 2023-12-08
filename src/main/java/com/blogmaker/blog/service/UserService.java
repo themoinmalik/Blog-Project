4b5d9e5050d9aa04f300f6b83e27b2a02c894d68
Moin Malik <themoinmalik@gmail.com>
2023-12-08 22:10:09 +0530

    new code added

--- src/main/java/com/blogmaker/blog/service/UserService.java
+++ src/main/java/com/blogmaker/blog/service/UserService.java
@@ -1,19 +1,16 @@
 package com.blogmaker.blog.service;
 
-
 import com.blogmaker.blog.dtos.UserDTO;
 import com.blogmaker.blog.entity.User;
 import com.blogmaker.blog.exception.ResourceNotFoundException;
 import com.blogmaker.blog.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class UserService {
@@ -26,7 +23,6 @@ public class UserService {
     }
 
     public ResponseEntity<User> findUserById(Long userId) {
-
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new ResourceNotFoundException("user not found"));
         return new ResponseEntity<>(user, HttpStatus.OK);
